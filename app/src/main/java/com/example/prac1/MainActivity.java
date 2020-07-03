2f699a673d48003d81d0126d7ebc2be67e7d5938
Kelly Fang <kellywmf-wm19@student.tarc.edu.my>
2020-07-03 14:55:21 +0800

    commit by kelly

--- app/src/main/java/com/example/prac1/MainActivity.java
+++ app/src/main/java/com/example/prac1/MainActivity.java
@@ -2,30 +2,63 @@ package com.example.prac1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static com.example.prac1.R.string.toast_message;
 
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+    private Button reset;
+    private Button mEvenOdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        reset = (Button) findViewById(R.id.button_zero);
+        mEvenOdd = (Button) findViewById(R.id.button_count);
     }
 
     public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this, toast_message, Toast.LENGTH_SHORT);
         toast.show();
     }
 
-    public void countUp(View view) {
+    public void countUp(View view){
         ++mCount;
-        if (mShowCount != null)
+        if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+
+    public void zero(View view){
+        if (mShowCount != null) {
+            int mZero = 0;
+            mShowCount.setText(Integer.toString(mZero));
+            mCount = 0;
+        }
+    }
+
+    public Button getReset() {
+        return reset;
+    }
+
+    public void setReset(Button reset) {
+        this.reset = reset;
+    }
+
+    public Button getmEvenOdd() {
+        return mEvenOdd;
+    }
+
+    public void setmEvenOdd(Button mEvenOdd) {
+        this.mEvenOdd = mEvenOdd;
     }
 }
